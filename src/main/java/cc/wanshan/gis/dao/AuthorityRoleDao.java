24abcfbed8dbbc4d67ba6bc18152656574cb7917
renmaoyan <1169000319@qq.com>
2019-05-24 10:08:29 +0800

    mvn package update

--- src/main/java/cc/wanshan/gis/dao/AuthorityRoleDao.java
+++ src/main/java/cc/wanshan/gis/dao/AuthorityRoleDao.java
@@ -1,4 +1,4 @@
-package cc.wanshan.gisdev.dao;
+package cc.wanshan.gis.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
@@ -7,19 +7,20 @@ import org.springframework.stereotype.Component;
  * @author Li Cheng
  * @date 2019/5/18 17:24
  */
+
 /**
- author_role_id character varying(32) NOT NULL DEFAULT replace(uuid_generate_v1()::text,'-',''),
- author_id character varying(32) COLLATE pg_catalog."default" NOT NULL,
- role_id character varying(32) COLLATE pg_catalog."default" NOT NULL,
- CONSTRAINT fkaukq8hfdqf5vjcrwgps5tojpq FOREIGN KEY (role_id)
- REFERENCES public.tb_role (role_id) MATCH SIMPLE
- ON UPDATE NO ACTION
- ON DELETE NO ACTION,
- CONSTRAINT fkgfb2hd9j9ryp5jpxw5q2xiubj FOREIGN KEY (author_id)
- REFERENCES public.tb_authority (author_id) MATCH SIMPLE
- ON UPDATE NO ACTION
- ON DELETE NO ACTION,
- CONSTRAINT tb_authority_role_pkey PRIMARY KEY (author_role_id)
+ * author_role_id character varying(32) NOT NULL DEFAULT replace(uuid_generate_v1()::text,'-',''),
+ * author_id character varying(32) COLLATE pg_catalog."default" NOT NULL,
+ * role_id character varying(32) COLLATE pg_catalog."default" NOT NULL,
+ * CONSTRAINT fkaukq8hfdqf5vjcrwgps5tojpq FOREIGN KEY (role_id)
+ * REFERENCES public.tb_role (role_id) MATCH SIMPLE
+ * ON UPDATE NO ACTION
+ * ON DELETE NO ACTION,
+ * CONSTRAINT fkgfb2hd9j9ryp5jpxw5q2xiubj FOREIGN KEY (author_id)
+ * REFERENCES public.tb_authority (author_id) MATCH SIMPLE
+ * ON UPDATE NO ACTION
+ * ON DELETE NO ACTION,
+ * CONSTRAINT tb_authority_role_pkey PRIMARY KEY (author_role_id)
  **/
 @Mapper
 @Component
