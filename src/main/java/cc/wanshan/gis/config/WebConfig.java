2baabc0d68a8512fd3a7852e027aa0b0179e23d5
renmaoyan <1169000319@qq.com>
2019-06-03 15:30:03 +0800

    add cors url

--- src/main/java/cc/wanshan/gis/config/WebConfig.java
+++ src/main/java/cc/wanshan/gis/config/WebConfig.java
@@ -28,6 +28,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://172.24.4.11:8000")
+                .allowedOrigins("http://192.168.1.119:8090")
+                .allowedOrigins("http://192.168.1.123:8090")
+                .allowedOrigins("http://192.168.1.121:8090")
+                .allowedOrigins("http://192.168.1.27:8090")
+
+                .allowedOrigins("http://192.168.1.123:8080")
+
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .allowedMethods("*")
