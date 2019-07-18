d972e86bf07eb787169850f5012dbf8ce7d96398
李成 <13201553110@163.com>
2019-07-18 13:53:00 +0800

    xxx

--- src/main/java/cc/wanshan/gis/service/geoserver/UserPropsService.java
+++ src/main/java/cc/wanshan/gis/service/geoserver/UserPropsService.java
@@ -1,9 +1,17 @@
 package cc.wanshan.gis.service.geoserver;
 
+import cc.wanshan.gis.entity.geoserver.UserProps;
+
 /**
  * @author Li Cheng
  * @date 2019/7/18 9:53
  */
 public interface UserPropsService {
-
+  /**
+   * description:根据用户名查找userProps
+   *
+   * @param username 用户名
+   * @return cc.wanshan.gis.entity.geoserver.UserProps
+   **/
+  UserProps findUserPropsByUsername(String username);
 }
