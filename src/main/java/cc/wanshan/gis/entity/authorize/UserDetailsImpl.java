2d7266c882586593680a3f1b337d36bc071eec11
李成 <13201553110@163.com>
2019-07-25 08:52:55 +0800

    xx

--- src/main/java/cc/wanshan/gis/entity/authorize/UserDetailsImpl.java
+++ src/main/java/cc/wanshan/gis/entity/authorize/UserDetailsImpl.java
@@ -7,7 +7,6 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
 import java.util.Collections;
